9ff026aa98a8c1bf2740a449da53944000118db2
jkeppers <justin@keppers.org>
2014-11-21 13:35:45 -0600

    jsonp is working

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,16 +10,32 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 
-.controller("PotsCtrl", function($scope, $http) {
-  $http.get('view1/test.json').
-      success(function(data, status, headers, config) {
-        $scope.pots = data;
-      }).
-      error(function(data, status, headers, config) {
-        // log error
-      });
+
+.controller("PotsCtrl", function($scope, $http, $timeout, $log) {
+        var getjson = function() {
+            $http.jsonp('http://192.168.1.4/latest?callback=JSON_CALLBACK').
+                success(function(data, status, headers, config) {
+                    $scope.pots = data;
+                }).
+                error(function(data, status, headers, config) {
+                    $log.warn('failed to retreive json file');
+                });
+            var timer = $timeout(getjson, 2000);
+            $scope.$on(
+                "$destroy",
+                function( event ) {
+
+                    $timeout.cancel( timer );
+
+                }
+            );
+
+        }
+
         $scope.$watch('boil', function(value){
             $(window).resize();
         });
+        getjson()
+
     });
 
