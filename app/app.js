480105b3a64312af62fbe1c44c70e4577b720e93
jkeppers <justin@keppers.org>
2014-11-26 15:39:02 -0600

    buttons start building config, slider bar worksish

--- app/app.js
+++ app/app.js
@@ -5,7 +5,9 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  'myApp.version',
+  'vr.directives.slider',
+  'angular.filter'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -12,8 +12,10 @@ angular.module('myApp.view1', ['ngRoute'])
 
 
 .controller("PotsCtrl", function($scope, $http, $timeout, $log) {
+        $scope.pots = null;
         var getjson = function() {
-            $http.jsonp('http://192.168.1.4/latest?callback=JSON_CALLBACK').
+            //$http.jsonp('http://192.168.1.4/latest?callback=JSON_CALLBACK').
+            $http.get('view1/test.json').
                 success(function(data, status, headers, config) {
                     $scope.pots = data;
                 }).
@@ -35,6 +37,38 @@ angular.module('myApp.view1', ['ngRoute'])
         $scope.$watch('boil', function(value){
             $(window).resize();
         });
+
+        $scope.submitData =  function ()
+        {
+            if (!angular.isDefined($scope.boil)){return;}
+            var data = {};
+            var whichIsOn = $scope.boil.split("_");
+            var whichStatus = whichIsOn[1];
+            var whichPot = whichIsOn[0];
+            var pot;
+            for (pot in $scope.pots){
+                if (whichPot == pot) {
+                    if (whichStatus == "boil"){
+                        data[pot + "_target"] = 300 + $scope.boilRate;
+                    }else if (whichStatus == "168"){
+                        data[pot + "_target"] = 168;
+                    }
+                }
+                else {
+                    data[pot+"_target"] = 0;
+                }
+            }
+            $log.warn($scope.boil);
+            $log.warn(data);
+
+
+        };
+
+        $scope.updateradio = function () {
+            $scope.$broadcast('refreshSlider');
+            $scope.submitData();
+        }
+
         getjson()
 
     });
